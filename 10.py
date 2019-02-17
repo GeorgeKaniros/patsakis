5fd45d8c3b34d3ab37dfe94f4d93acc68b004947
GeorgeKaniros <47721326+GeorgeKaniros@users.noreply.github.com>
2019-02-17 20:18:13 +0200

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,18 @@
+﻿from bs4 import BeautifulSoup
+import requests
+
+diefthinsi=input("Δωσε μια διευθυνση για ανιχνευση συνδεσμων και αλλαγων γραμμης")
+selida=requests.get(diefthinsi)
+kodikas=BeautifulSoup(selida.text, 'lxml')
+
+all_gram=0
+syndesmoi=0
+
+syn=kodikas.find_all('a')
+syndesmoi=len(syn)
+brs=kodikas.find_all('br')
+ps=kodikas.find_all('p')
+all_gram=len(ps)+len(brs)
+
+print("Συνδεσμοι:",syndesmoi)
+print("Αλλαγες γραμμης:",all_gram)
