001e5b6bd0f758e479e6b6cb2014d83e0a017614
GeorgeKaniros <47721326+GeorgeKaniros@users.noreply.github.com>
2019-02-17 20:17:28 +0200

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,96 @@
+def times(x):
+    x = str(x) + "*"
+    return x
+def minus(x):
+    x = str(x) + "-"
+    return x
+def plus(x):
+    x = str(x) + "+"
+    return x
+def operation(y, clct, ar):
+    if clct == "*":
+        return ar * y
+    elif clct == "-":
+        return y - ar
+    elif clct == "+":
+        return ar + y
+def zero(x=10):
+    if x == 10:
+        return 0
+    else:
+        y = 0
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def one(x=10):
+    if x == 10:
+        return 1
+    else:
+        y = 1
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def two(x=10):
+    if x == 10:
+        return 2
+    else:
+        y = 2
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def three(x=10):
+    if x == 10:
+        return 3
+    else:
+        y = 3
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def four(x=10):
+    if x == 10:
+        return 4
+    else:
+        y = 4
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def five(x=10):
+    if x == 10:
+        return 5
+    else:
+        y = 5
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def six(x=10):
+    if x == 10:
+        return 6
+    else:
+        y = 6
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def seven(x=10):
+    if x == 10:
+        return 7
+    else:
+        y = 7
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def eight(x=10):
+    if x == 10:
+        return 8
+    else:
+        y = 8
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
+def nine(x=10):
+    if x == 10:
+        return 9
+    else:
+        y = 9
+        ar = int(x[0])
+        clct = x[1]
+        print(operation(y, clct, ar))
